458651670c33e1fde5cb6cc58e3c934e05609a6a
sushanth <sushanth.nandan@games24x7.com>
2023-10-12 22:28:04 +0530

    added a setter in Device.java

--- src/main/java/com/appium/device/Device.java
+++ src/main/java/com/appium/device/Device.java
@@ -2,9 +2,11 @@ package com.appium.device;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
+import lombok.Setter;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Getter
+@Setter
 public class Device {
     public String name;
     public String udid;
